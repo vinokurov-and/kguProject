3537abd38c0d93d62a79d51f8db9ea23b8513a47
Алина Никитина <144150455+cherepashhca@users.noreply.github.com>
2023-10-16 17:52:44 +0300

    Добавлен Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+     <Header>
+     </Header>
     </div>
   );
 }
