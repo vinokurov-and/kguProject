452e54e0496df1fa907d3c020acba307a4216152
Kristina Shishkina <klikklok2017@mail.ru>
2023-10-27 16:08:41 +0300

    высота компонента

--- src/components/LoadingIndicator/LoadingIndicator.js
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export const LoadingIndicator = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
       <CircularProgress />
     </div>
   );
