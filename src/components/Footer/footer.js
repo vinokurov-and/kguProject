10a1036e5b9a49fb87f1316b516f0973380bf193
White-Breadd <Egor542Tkachev@gmail.com>
2023-10-31 14:04:24 +0300

    футер

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,22 +3,12 @@ import "./style.css";
 const Footer = () => {
     return (   
    <body>
-     <div class="line"></div>
-     <div>
-
-    </div>
+    <div class="line"></div>
     <div> 
         <h1>Разработано студентами КГУ им. Циолковского</h1> 
         <h2>Copyright © 2023. Все права защищены.</h2>
     </div>
-   
-    <div>
-
-
-    </div>
-   </body>
-   
-        
+   </body>       
     );
 }
 
