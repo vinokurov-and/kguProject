e88f2cd290a73f2c6dc6eeff4ca1f5ddd3d04a55
TheDart216 <33442266887799@mail.ru>
2023-12-25 18:25:34 +0300

    переделал курент дейс

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Footer from './components/Footer/Footer';
 import { Header } from './components/Header';
 import { Logo } from './components/Logo'
 import { SocialIcons } from './components/SocialIcons';
-
+import { CurrentDay } from './components/CurrentDay/CurrentDay'
 
 function App() {
   return (
-    <div className="App"
+    <div className="App">
       <Header>
       <Logo/>     
       <SocialIcons />
       </Header>
       <Footer />
+      <CurrentDay />
     </div>
   );
 }
--- src/components/CurrentDay/CurrentDay.jsx
+++ src/components/CurrentDay/CurrentDay.jsx
@@ -1,20 +1,12 @@
-import React, { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 
-
 export const CurrentDay = () => {
-    const [currentDay, setCurrentDay] = useState('');
-    
-    useEffect(() => {
-        const dayOfWeek = new Date().toLocaleString('ru-RU', { weekday: 'long' });
-        setCurrentDay(dayOfWeek);
-    }, []);
-    
+    const currentDay = new Date().toLocaleString('ru-RU', { weekday: 'long' });
     return (
-        <div>
-        <Typography variant="h3">
-            Сегодня: {currentDay}
-        </Typography>
+        <div style={{ position: 'fixed', bottom: '0', left: '0', padding: '10px' }}>
+            <Typography variant="h6" component="h1">
+                Сегодня: {currentDay}
+            </Typography>
         </div>
     );
-}
+};
