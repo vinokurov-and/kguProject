9340a0faf32ae60cfc5024ee21308643c0186499
Denitrust <144033273+Denitrust@users.noreply.github.com>
2024-01-13 14:04:54 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import { Header } from './components/Header';
-import { Main } from './components/Main';
 import { Logo } from './components/Logo'
 import { SocialIcons } from './components/SocialIcons';
-import { CurrentDay } from './components/CurrentDay/CurrentDay'
+import { WeekType } from './components/WeekType';
+import { CurrentWeek } from './components/WeekType';
+
 
 function App() {
   return (
     <div className="App">
-     <Header>
-     </Header>
-     <Main>
-      <CurrentDay/>
-     </Main>
-     
       <Header>
+      <WeekType weekTypee={CurrentWeek%2===0} />
       <Logo/>     
       <SocialIcons />
       </Header>
       <Footer />
-      <CurrentDay />
     </div>
   );
 }
