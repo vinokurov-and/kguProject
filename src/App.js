b2fba2dab485fbfde83305e4303c5ae8c5c300c2
Denitrust <144033273+Denitrust@users.noreply.github.com>
2024-01-13 14:03:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,17 @@ import { Main } from './components/Main';
 import { Logo } from './components/Logo'
 import { SocialIcons } from './components/SocialIcons';
 import { CurrentDay } from './components/CurrentDay/CurrentDay'
+import { LessonType } from './components/LessonType';
 
 function App() {
   return (
     <div className="App">
      <Header>
-     </Header>
-     <Main>
-      <CurrentDay/>
-     </Main>
-     
-      <Header>
+      <h2><LessonType /></h2>
       <Logo/>     
       <SocialIcons />
       </Header>
       <Footer />
-      <CurrentDay />
     </div>
   );
 }
